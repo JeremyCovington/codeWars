cb347bedcffc795e50e922923659162c71ef8329
Jeremy <jeremylc153@gmail.com>
2024-05-23 14:41:11 -0600

    adding 8 kyu

--- JS/8s/reversedSequence.js
+++ JS/8s/reversedSequence.js
@@ -1,7 +1,22 @@
+// Build a function that returns an array of integers from n to 1 where n>0.
 
+// Example : n=5 --> [5,4,3,2,1]
 
 // My Soltuion
-
+const reverseSeq = n => {
+  let arr = []
+  for (i = n; i > 0; i--) {
+    arr.push(i)
+    --n
+  };
+  return arr
+};
 
 
 // Best Solution
+const reverseSeq = n => {
+  let arr = [];
+  for (let i = n; i > 0; i--) {
+    arr.push(i);
+  } return arr;
+};
